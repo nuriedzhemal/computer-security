4ed76ad407e9e00e2863ed2b9bf441e0215e7294
Nurie Dzhemal <138439607+nuriedzhemal@users.noreply.github.com>
2024-02-26 09:34:52 +0200

    Update caesarCypherDecryption.c

--- caesarCypherDecryption.c
+++ caesarCypherDecryption.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void decrypt(char str[], int key)
+void decrypt(char cypher[], int key)
 {
-    for (int i =0; str[i] !='\n' && str[i] != '\0'; i++){
-        char current = str[i];
+    for (int i =0; cypher[i] !='\n' && cypher[i] != '\0'; i++){
+        char current = cypher[i];
         if('A'<= current && current <='Z')
-        str[i] = 'A'+ (current - 'A' - key)%26;
+        cypher[i] = 'A'+ (current - 'A' - key)%26;
 
     }
 
@@ -14,17 +14,17 @@ void decrypt(char str[], int key)
 
 int main(){
 
-   char str[100];
+   char cypher[100];
    int key;
    
  printf("Enter text: ");
- scanf("%s", str);
+ scanf("%s", cypher);
  printf("Enter key value: ");
  scanf("%d", &key);
     
     
-  decrypt(str, key);
- printf("Decrypted text: %s\n", str);
+  decrypt(cypher, key);
+ printf("Decrypted text: %s\n", cypher);
 
     return EXIT_SUCCESS;
 
